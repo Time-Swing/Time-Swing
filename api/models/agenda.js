10d29b7d9b94185df30f2bc0d0be479b7fbbd6c7
Alex-Pro-commit <73656348+Alex-Pro-commit@users.noreply.github.com>
2021-11-09 20:19:15 -0500

    Agenda Controllers

--- api/models/agenda.js
+++ api/models/agenda.js
@@ -1,36 +1,59 @@
-' use strict'
+" use strict";
 
-const { Model } = require('sequelize');
+const { Model } = require("sequelize");
 
-module.exports = (sequelize,DataTypes)=>{
-    class Agenda extends Model{}
+module.exports = (sequelize, DataTypes) => {
+	class Agenda extends Model {}
 
-    Agenda.init({
-        timeStart:{
-            type:DataTypes.DATE,
-            validate:{
-                len:[8,250],
-                notEmpty:true,
-            }
-        },
-        timeEnd:{
-            type:DataTypes.DATE,
-            validate:{
-                len:[8,250],
-                notEmpty:true,
-            }
-        }
-    },{
-        sequelize,
-        modelName:'agenda'
-    });
+	Agenda.init(
+		{
+			timeStart: {
+				type: DataTypes.DATE,
+				validate: {
+					len: [8, 250],
+					notEmpty: true,
+				},
+			},
+			timeEnd: {
+				type: DataTypes.DATE,
+				validate: {
+					len: [8, 250],
+					notEmpty: false,
+				},
+			},
+			title: {
+				type: DataTypes.STRING,
+				validate: {
+					len: [3, 250],
+					notEmpty: true,
+				},
+			},
+			content: {
+				type: DataTypes.STRING,
+				validate: {
+					notEmpty: false,
+				},
+			},
+			address: {
+				type: DataTypes.STRING,
+				validate: {
+					len: [3, 250],
+					notEmpty: false,
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: "agenda",
+		}
+	);
 
-    // Genre.associate = (models) => {
-    //     // associations can be defined here
-    
-    //     // This will add genreId to the Movie model and table
-    //     models.Genre.hasMany(models.Movie)
-    // };
-    
-    return Agenda;
+	// Genre.associate = (models) => {
+	//     // associations can be defined here
+
+	//     // This will add genreId to the Movie model and table
+	//     models.Genre.hasMany(models.Movie)
+	// };
+
+	return Agenda;
 };
