fa32cb7dd9da547752cc80220be450c48e550504
Alex-Pro-commit <73656348+Alex-Pro-commit@users.noreply.github.com>
2021-11-19 14:51:56 -0500

    4th time,eventually!!!

--- client/src/pages/agenda/PostAgenda.js
+++ client/src/pages/agenda/PostAgenda.js
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
 import React from "react";
 import { Redirect } from "react-router-dom";
-// import { DatePicker } from "rsuite";
-=======
-import React from 'react';
-import { Redirect } from 'react-router-dom';
 import Autocomplete from "react-google-autocomplete";
->>>>>>> d64bae4d2bc4903ad9406aea135ede2c461a2012
 
-const API_KEY = process.env.REACT_APP_API_KEY
-console.log(API_KEY)
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY);
 class PostAgenda extends React.Component {
-<<<<<<< HEAD
 	state = {
 		error: false,
 		success: false,
+		more: "none",
+		moreOrless: "More",
 		content: {
 			title: "",
 			timeStart: "",
@@ -38,47 +33,14 @@ class PostAgenda extends React.Component {
 			};
 		});
 	};
-=======
-  state = {
-    error: false,
-    success: false,
-    more:'none',
-    moreOrless:"More",
-    content:{
-        title:'',
-        timeStart:'',
-        timeEnd:'',
-        content:'',
-        address:''
-    }, 
-}
 
-
-  handleChanged = (event) =>{
-    const {name, value} = event.target
-    console.log(name,value)
-    // console.log("name: "+name+ "...value:"+value)
-    this.setState(preData=>{
-      return {
-          ...preData,
-          content:{
-              ...preData.content,
-              [name]:value
-          }
-      }
-      
-  })
-}
-
-  showMore = (e) =>{
-    if(this.state.more === ''){
-      this.setState({more:'none',moreOrless:'More'})
-    }
-    else{
-      this.setState({more:'',moreOrless:'Less'})
-    }
-  }
->>>>>>> d64bae4d2bc4903ad9406aea135ede2c461a2012
+	showMore = (e) => {
+		if (this.state.more === "") {
+			this.setState({ more: "none", moreOrless: "More" });
+		} else {
+			this.setState({ more: "", moreOrless: "Less" });
+		}
+	};
 
 	saveAgenda = (event) => {
 		console.log(JSON.stringify(this.state.content));
@@ -121,7 +83,6 @@ class PostAgenda extends React.Component {
 			);
 		}
 
-<<<<<<< HEAD
 		return (
 			<div>
 				{errorMessage}
@@ -135,17 +96,7 @@ class PostAgenda extends React.Component {
 					/>
 				</div>
 				<br />
-				<div>
-					<input
-						type="text"
-						placeholder="Address"
-						name="address"
-						value={this.state.content.address}
-						onChange={this.handleChanged}
-					/>
-					<br />
-				</div>
-				<br />
+
 				<div>
 					<input
 						type="datetime-local"
@@ -155,25 +106,55 @@ class PostAgenda extends React.Component {
 					/>
 				</div>
 				<br />
-				<div>
-					<input
-						type="datetime-local"
-						name="timeEnd"
-						value={this.state.content.timeEnd}
-						onChange={this.handleChanged}
-					/>
-				</div>
+
+				<button onClick={this.showMore}>{this.state.moreOrless}</button>
 				<br />
-				<div>
-					<input
-						type="text"
-						name="content"
-						placeholder="Content"
-						value={this.state.content.content}
-						onChange={this.handleChanged}
-					/>
+				<div style={{ display: this.state.more }}>
+					<div>
+						<br />
+						<Autocomplete
+							apiKey={API_KEY}
+							options={{
+								types: ["address"],
+								componentRestrictions: { country: "us" },
+							}}
+							onPlaceSelected={(place) => {
+								console.log(place.formatted_address);
+								this.setState((preData) => {
+									return {
+										...preData,
+										content: {
+											...preData.content,
+											["address"]: place.formatted_address,
+										},
+									};
+								});
+							}}
+						/>
+						;
+						<br />
+					</div>
 					<br />
+					<div>
+						<input
+							type="datetime-local"
+							name="timeEnd"
+							value={this.state.content.timeEnd}
+							onChange={this.handleChanged}
+						/>
+					</div>
+					<br />
+					<div>
+						<textarea
+							name="content"
+							placeholder="Content"
+							value={this.state.content.content}
+							onChange={this.handleChanged}
+						/>
+						<br />
+					</div>
 				</div>
+
 				<br />
 				<div>
 					<button className="btn btn-primary" onClick={this.saveAgenda}>
@@ -183,87 +164,6 @@ class PostAgenda extends React.Component {
 			</div>
 		);
 	}
-=======
-    return (
-      <div>
-        { errorMessage }
-        <div>
-          <input 
-            type="text" 
-            placeholder="Type the title" 
-            name="title"
-            value={this.state.content.title}
-            onChange={this.handleChanged}
-          />
-        </div>
-        <br/>
-
-        <div>
-          <input 
-              type="datetime-local" 
-              name="timeStart"
-              value={this.state.content.timeStart}
-              onChange={this.handleChanged}
-            />
-        </div>
-        <br/>
-
-
-        <button onClick={this.showMore}>{this.state.moreOrless}</button>
-        <br/>
-        <div style={{display:this.state.more}}>
-        <div>
-          <br/>
-        <Autocomplete
-         apiKey={API_KEY}
-         options={{
-          types: ["address"],
-          componentRestrictions: { country: "us" }
-        }}
-        onPlaceSelected={(place) => {
-          console.log(place.formatted_address)
-          this.setState(preData=>{
-            return {
-                ...preData,
-                content:{
-                    ...preData.content,
-                    ["address"]:place.formatted_address
-                }
-            }
-        })
-        }}        
-/>;
-          <br/>
-        </div>
-        <br/>
-        <div>
-        <input 
-              type="datetime-local" 
-              name="timeEnd"
-              value={this.state.content.timeEnd}
-              onChange={this.handleChanged}
-            />
-        </div>
-        <br/>
-        <div>
-          <textarea 
-            name="content"
-            placeholder="Content" 
-            value={this.state.content.content}
-            onChange={this.handleChanged}
-          />
-          <br/>
-        </div>
-        </div>
-
-        <br/>
-        <div>
-          <button className="btn btn-primary" onClick={this.saveAgenda}>Save Post</button>
-        </div>
-      </div>
-    );
-  }
->>>>>>> d64bae4d2bc4903ad9406aea135ede2c461a2012
 }
 
 export default PostAgenda;
